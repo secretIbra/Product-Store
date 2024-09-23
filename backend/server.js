e524993f1ec9766c5d976395349fcc30fc885174
Ibrahim <hema.tarek2000@gmail.com>
2024-09-23 23:58:26 +0300

    frontend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); // allows us to accept json data in the req body
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port " + PORT);
+});
+
+//
